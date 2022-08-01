cf4a7b862546b8bbcf1f791ed7084c0dd8b6392f
Fito96 <86424085+Fito96@users.noreply.github.com>
2022-08-01 15:34:38 -0500

    Add files via upload

---
+++ peer_graded_notebook.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook in IBM Watson Studio
+
+# **Rodolfo Navarro Olmos**
+
+# *I'm interested in Data Science because I'm very interested in ML models and their applications to solve real complex problems*
+
+# ### This code sums 2 + 2
+
+# In[1]:
+
+
+2 + 2
+
+
+# [IBM](www.ibm.com)
+# 
+# * one
+# * two
+# * three
+# 
+# | Stretch/Untouched | ProbDistribution | Accuracy |
+# | --- | --- | --- |
+# | Stretched | Gaussian | .843 |
+
+# In[ ]:
+
+
+
+
